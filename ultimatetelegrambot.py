ea374e2b31007846f9e90561eafbb0e6cd28c10a
Mohamed Reda <61638355+MohamedReda2003@users.noreply.github.com>
2023-07-11 09:13:36 +0100

    Add files via upload

---
+++ ultimatetelegrambot.py
@@ -0,0 +1,157 @@
+import subprocess
+
+subprocess.check_call([ "pip", "install", "pymongo"])
+subprocess.check_call([ "pip", "install", "playwright"])
+subprocess.check_call([ "pip", "install", "pyTelegramBotAPI"])
+
+
+
+import telebot
+from playwright.sync_api import Playwright, sync_playwright, expect
+from pymongo import MongoClient
+import time
+import threading
+import sys
+
+subprocess.check_call([ "playwright", "install", "chromium"])
+subprocess.check_call([ "playwright", "install-deps"]) 
+
+
+
+  
+TOKEN = '5819911264:AAFerjEgyS3sY8S3TBGWr1hnqiKuXR0UcYU'
+CHAT_ID1 = '-1001979800418'
+CHAT_ID2 = '-1001988500858'
+bot = telebot.TeleBot(TOKEN)
+
+
+#cluster=MongoClient("mongodb+srv://fiverrautomation:he3eyetR@cluster0.pshiyd4.mongodb.net/?retryWrites=true&w=majority")
+#db =cluster["ENSA2023"]
+#collection=db["modules-ENSA2023"]
+#bad_collection=db['bab_language']
+#affiche=collection.find_one()
+error_flag = False
+
+
+def run(collection, affiche):
+    with sync_playwright() as playwright:
+
+   
+        browser = playwright.chromium.launch()
+        context = browser.new_context()
+        # Open new page
+        page = context.new_page()
+    
+        page.goto("https://ent.ensa-tetouan.ac.ma/?home/index", wait_until="load")
+        page.wait_for_load_state("networkidle")
+        page.is_visible('//html/body/doctype/section[1]/div/div/div[2]/div/div/h2')
+
+
+
+    
+        page.locator("[placeholder=\"Email\"]").click()
+        page.locator("[placeholder=\"Email\"]").fill("zhar.mohamedreda@etu.uae.ac.ma")
+
+        page.locator("[placeholder=\"Mot de passe\"]").click()
+
+        page.locator("[placeholder=\"Mot de passe\"]").fill("he3eyetR2003")
+      
+        page.locator("text=S'authentifier").click()
+    
+    
+
+        page.wait_for_url("https://ent.ensa-tetouan.ac.ma/?home/home")
+        page.locator("text=Mon résultat Consultation de vos notes. >> img").click()
+        page.wait_for_url("https://ent.ensa-tetouan.ac.ma/?myscore/index")
+        page.is_visible("//html/body/doctype/section/div/div[2]/div[1]/div/table/thead/tr/th[1]")
+        te = page.wait_for_selector('//html/body/doctype/section/div/div[2]/div[1]/div/table', timeout=10000).inner_text()
+            
+        result = te.split('\t')
+        matter=result[-3]
+        print('\a')
+        if matter != "":
+            a=False
+            modules=affiche['Module affiches']
+            for module in modules :
+                if matter==module or matter==module.replace('é','e'):
+                    #bot.send_message('1798052577', matter)
+                    a=True
+                    break
+            if a==False:
+                mfg= f"{matter} est affiche!"
+                new_list=modules+[f'{matter}']
+                newvalues ={ "$set": { "Module affiches": new_list } }
+                collection.update_one(affiche, newvalues)
+            else :
+                mfg=''
+        else:
+            mfg=''
+    
+        context.close()
+        browser.close()
+        return mfg
+
+
+
+print('success')
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+def check():
+    global error_flag
+    counter=0
+    while not error_flag:
+        #bot.send_message('1798052577', str(error_flag))
+        try:
+            with MongoClient("mongodb+srv://fiverrautomation:he3eyetR@cluster0.pshiyd4.mongodb.net/?retryWrites=true&w=majority") as cluster:
+                db = cluster["ENSA2023"]
+                collection = db["modules-ENSA2023-RATT"]
+                bad_collection = db['bab_language']
+                try:
+                    affiche = collection.find_one()
+                except :
+                    affiche={}
+
+                msg = run(collection, affiche)
+                if msg != '':
+                    try:
+                        bot.send_message(CHAT_ID1, msg)
+                    except :
+                        bot.send_message(CHAT_ID2, msg)
+                        
+
+            time.sleep(10)
+        except TypeError :
+            pass
+        except Exception as e :
+            error_flag=True 
+            bot.send_message('1798052577', e)
+            bot.send_message('1798052577', str(counter))
+
+        if counter%10==0:
+            bot.send_message('1798052577', str(counter))
+            
+            
+        counter+=1
+    
+    
+		
+    
+        
+def the_main():
+    global error_flag
+    scraping_thread = threading.Thread(target=bot.infinity_polling)
+    scraping_thread.daemon = True
+    scraping_thread.start()
+
+             
+
+
+
+
+if __name__ == '__main__':
+    the_main()
+    # bot.infinity_polling()
+    check()
---
+++ ultimatetelegrambot2.py
@@ -0,0 +1,141 @@
+import subprocess
+subprocess.check_call([ "pip", "install", "pymongo"])
+subprocess.check_call([ "pip", "install", "playwright"])
+subprocess.check_call([ "pip", "install", "pyTelegramBotAPI"])
+subprocess.check_call([ "pip", "install", "selenium"])
+subprocess.check_call([ "pip", "install", "webdriver_manager"])
+
+
+from telebot import types
+import telebot
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from pymongo import MongoClient
+import time
+import threading
+
+
+#subprocess.check_call([ "playwright", "install", "chromium"])
+
+TOKEN = '5819911264:AAFerjEgyS3sY8S3TBGWr1hnqiKuXR0UcYU'
+CHAT_ID = '-1001988500858'
+print("Here we go")
+bot = telebot.TeleBot(TOKEN)
+
+
+cluster=MongoClient("mongodb+srv://fiverrautomation:he3eyetR@cluster0.pshiyd4.mongodb.net/?retryWrites=true&w=majority")
+db =cluster["ENSA2023"]
+collection=db["modules-ENSA2023"]
+bad_collection=db['bab_language']
+affiche=collection.find_one()
+
+def run():
+    options = webdriver.ChromeOptions()
+    
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument('--disable-dev-shm-usage')
+    # options.binary_location = '/usr/bin/chromium'
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    
+    print('the page')
+    driver.get("https://ent.ensa-tetouan.ac.ma/?home/index")
+    
+    wait = WebDriverWait(driver, 10)
+
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[placeholder="Email"]'))).click()
+    driver.find_element(By.CSS_SELECTOR, '[placeholder="Email"]').send_keys("zhar.mohamedreda@etu.uae.ac.ma")
+    
+    driver.find_element(By.CSS_SELECTOR, '[placeholder="Mot de passe"]').click()
+    driver.find_element(By.CSS_SELECTOR, '[placeholder="Mot de passe"]').send_keys("he3eyetR2003")
+    
+    driver.find_element(By.XPATH, "/html/body/doctype/section[1]/div/div/div[2]/div/div/form/div/div[3]/button").click()
+    print('logged')
+    
+    wait.until(EC.url_to_be("https://ent.ensa-tetouan.ac.ma/?home/home"))
+    
+    driver.find_element(By.XPATH, "/html/body/doctype/section/div/div/div[2]/div/h5/a").click()
+    
+    wait.until(EC.url_to_be("https://ent.ensa-tetouan.ac.ma/?myscore/index"))
+    # time.sleep(30)
+    table_element = wait.until(EC.presence_of_element_located((By.XPATH, '//html/body/doctype/section/div/div[2]/div[1]/div/table')))
+    te = table_element.get_attribute("innerText")
+    
+    try:
+        with open("notes.txt", "r") as file:
+            file_contents = file.read()
+    except:
+        file_contents = ""
+    
+    if file_contents == te:
+        print('pas de nouvelles ...')
+        mfg = ''
+    elif file_contents != te:
+        result = te.split('\t')
+        matter = result[-3]
+        print('\a')
+        if matter != "":
+            a=False
+            modules=affiche['Module affiches']
+            for module in modules :
+                if matter==module:
+                    bot.send_message('1798052577', matter)
+                    a=True
+                    break
+            if a==False:
+                mfg= f"{matter} est affiche!".replace('é','e')
+                new_list=modules+[f'{matter}']
+                newvalues ={ "$set": { "Module affiches": new_list } }
+                collection.update_one(affiche, newvalues)
+            else :
+                mfg=''
+        else:
+            mfg=''
+        
+        print(mfg)
+            # sendwhat(message)
+            # with open("notes.txt", "w") as file:
+            #   file.write(te)
+    
+        # ---------------------
+        driver.quit()
+        return mfg
+print('success')
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    print(message.text)
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+#def greet_user(messages):
+    #for message in messages:
+        #for new_member in message.new_chat_members:
+            #bot.send_message(message.chat.id, f'Welcome {new_member.first_name} to the group!')
+
+
+
+#bot.set_update_listener(greet_user)
+
+def check():
+    while True :
+        print('running...')
+        try:
+            msg=run()
+            bot.send_message(CHAT_ID, msg)
+        except Exception as e :
+            print(e)
+            bot.send_message('1798052577', e)
+            pass
+		
+        time.sleep(50)
+scraping_thread = threading.Thread(target=check)
+scraping_thread.daemon = True
+scraping_thread.start()
+
+
+if __name__ == '__main__':
+
+	bot.infinity_polling()
